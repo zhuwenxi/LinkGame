85b8ef7878011f32ac2f92eaa4b9fd183410f59c
zhuwenxi <zhuwenxi8935@gmail.com>
2014-02-19 23:08:31 +0800

    Prototype constructed.

--- src/Block.js
+++ src/Block.js
@@ -1,3 +1,5 @@
+var BLOCK_SELECTED = 'BLOCK_SELECTED';
+
 var Block = cc.Sprite.extend({
 	ctor:function(type){
 		this._super();
@@ -7,5 +9,62 @@ var Block = cc.Sprite.extend({
 	init:function(type){
 		var frameName = 'cocos' + ('00' + type).slice(-2) + '.png';
 		this.initWithSpriteFrameName(frameName);
+	},
+
+	containsTouchLocation:function (touch) {
+        var getPoint = touch.getLocation();
+
+        var lx = 0 | (getPoint.x -  this.getPosition().x);//this.getPositionX();
+        var ly = 0 | (getPoint.y -  this.getPosition().y);//this.getPositionY();
+        if(lx>-22.5 && lx<22.5 && ly>-22.5 && ly<22.5)
+            return true;
+        return false;
+    },
+
+	onTouchBegan:function(touch, event){
+		//cc.log(this.row + ',' + this.col);
+		if(this.containsTouchLocation(touch)){
+			gNotification.postNotification(BLOCK_SELECTED, this);
+			return true;
+		}
+		return false
+	},
+
+	onTouchMoved:function(touch, event){
+		//cc.log('Touch moved!');
+		return true;
+	},
+
+	destroyBlocks:function(frames){
+		var destroySprite = cc.Sprite.createWithSpriteFrameName('pattern_destroy_00.png');
+		destroySprite.setPosition(22.5, 22.5);
+		this.addChild(destroySprite);
+
+		var animation = cc.Animation.create(frames, 0.025);
+		destroySprite.runAction(cc.Animate.create(animation));
+		var sequence = cc.Sequence.create(cc.FadeOut.create(0.5), cc.CallFunc.create(this.removeSelf, this));
+		this.runAction(sequence);
+
+		gSharedEngine.playEffect(EFFECT_PATTERN_BOMB);
+		//this.runAction(cc.Sequence.create(cc.DelayTime.create(1.5), cc.CallFunc.create(this.removeSelf, this)));
+	},
+
+	removeSelf:function(){
+		this.getParent().removeChild(this);
+	},
+
+	onEnter:function(){
+		if(sys.platform == 'browser'){
+			cc.registerTargetedDelegate(1, true, this);
+		}
+		else{
+			cc.registerTargettedDelegate(1, true, this);
+		}
+		this._super();
+	},
+
+	onExit:function(){
+		cc.unregisterTouchDelegate(this);
+		this._super();
 	}
 });
--- src/GameScene.js
+++ src/GameScene.js
@@ -13,6 +13,20 @@ var GameScene = cc.Layer.extend({
 
 	blockBatchNode : null,
 
+	blockSelectedSpr : null,
+
+	//current selected block
+	previousBlock : null,
+
+	//destroy frames
+	destoryFrames : [],
+
+	//Totally number of blocks
+	numOfBlocks : 0,
+
+	//Timer tally
+	timerTally : 0,
+
 	ctor : function(){
 		this._super();
 		this.init();
@@ -20,8 +34,11 @@ var GameScene = cc.Layer.extend({
 
 	init : function(){
 		var winSize = director.getWinSize();
+		//Set block numbers
+		this.numOfBlocks = this.n_MatrixCol * this.n_MatrixRow;
+
 		//Set total game time
-		timeTotal = 60;
+		this.timeTotal = 15;
 
 		//Set background
 		var bgSprite = cc.Sprite.create('res/background.jpg');
@@ -45,25 +62,35 @@ var GameScene = cc.Layer.extend({
 		this.blockBatchNode = cc.SpriteBatchNode.create('res/baseResource.png', this.n_MatrixRow * this.n_MatrixCol * 2);
 		this.addChild(this.blockBatchNode);
 
+		//Pattern Selected Sprite
+		this.blockSelectedSpr = cc.Sprite.createWithSpriteFrameName('pattern_selected.png');
+		this.blockSelectedSpr.setPosition(cc.p(-100, -100));
+		this.addChild(this.blockSelectedSpr);
+
 		//Init block sprite and block position array
 		this.blocksSpr = this.initArray(this.n_MatrixRow, this.n_MatrixCol, null);
 		this.blocksPos = this.initArray(this.n_MatrixRow, this.n_MatrixCol, null);
 
+		gNotification.addObserver(this, this.selectBlock, BLOCK_SELECTED);
+
 		//Init Matrix
 		this.initMatrix();
+
+		//Updating the progress persistently
+		this.schedule(this.updateProgress, 0.15);
 	},
 
 	initProgress:function(){
 		var winSize = director.getWinSize();
 
 		this.progressBarBg = cc.Sprite.create('res/ProgressBarBack.png');
-		this.progressBarBg.setAnchorPoint(0.5, 0.5);
-		this.progressBarBg.setPosition(winSize.width / 2, 20);
+		this.progressBarBg.setAnchorPoint(0.0, 0.5);
+		this.progressBarBg.setPosition(32, 20);
 		this.addChild(this.progressBarBg);
 
 		this.progressBar = cc.Sprite.create('res/ProgressBarFront.png');
-		this.progressBar.setAnchorPoint(0.5, 0.5);
-		this.progressBar.setPosition(winSize.width / 2, 20);
+		this.progressBar.setAnchorPoint(0.0, 0.5);
+		this.progressBar.setPosition(32, 20);
 		this.addChild(this.progressBar);
 	},
 
@@ -86,6 +113,9 @@ var GameScene = cc.Layer.extend({
 		var randomNumber = 0 | Math.random() * 7;
 		this.blocksSpr[row][col] = new Block(randomNumber);
 		this.blocksSpr[row][col].setPosition(this.blocksPos[row][col].x, this.blocksPos[row][col].y);
+		this.blocksSpr[row][col].row = row;
+		this.blocksSpr[row][col].col = col;
+		this.blocksSpr[row][col].type = randomNumber;
 		this.blockBatchNode.addChild(this.blocksSpr[row][col]);
 	}, 
 
@@ -98,5 +128,80 @@ var GameScene = cc.Layer.extend({
 			}
 		}
 		return arr;
-	}
+	}, 
+
+	initDestroyFrames:function(){
+		var frame = null;
+		for(var i = 0 ; i < 18 ; i++){
+			frame = gSpriteFrameCache.getSpriteFrame("pattern_destroy_"+("00"+i).slice(-2)+".png");
+            this.mDestroyFrames.push(frame);
+		}
+	},
+
+	selectBlock:function(block){
+		if(this.previousBlock == null){
+			this.previousBlock = block;
+			this.blockSelectedSpr.setPosition(this.blocksPos[block.row][block.col]);
+		}
+		else{
+			//Destory the chosen two blocks
+			var previous = this.previousBlock;
+			var current = block;
+
+			if(this.checkBlocks(previous, current)){
+				previous.destroyBlocks(this.destroyFrames);
+				current.destroyBlocks(this.destroyFrames);
+
+				this.numOfBlocks = this.numOfBlocks - 2;
+
+				this.blockSelectedSpr.setPosition(cc.p(-100, -100));
+				this.previousBlock = null;
+
+				if(this.numOfBlocks <= 0){
+					//Game Win
+					this.showGameResult(true);
+				}
+			}
+			else{
+				this.previousBlock = block;
+				this.blockSelectedSpr.setPosition(this.blocksPos[block.row][block.col]);
+			}
+
+			// block.destroyBlocks(this.destroyFrames);
+			// this.previousBlock.destroyBlocks(this.destroyFrames);
+			// this.previousBlock = null;
+		}
+	}, 
+
+	//Check if this block and the previous one are the same type
+	checkBlocks:function(previous, current){
+		return true;
+	},
+
+	updateProgress:function(dt){
+		this.timerTally += dt;
+		var progressPercent = (this.timeTotal - this.timerTally) / this.timeTotal;
+
+		if(progressPercent < 0){
+			progressPercent = 0;
+		}
+
+		this.progressBar.setTextureRect(cc.rect(0, 0 , 257*progressPercent, 19));
+
+		if(progressPercent == 0){
+			//Game fail
+			this.showGameResult(false);
+		}
+	},
+
+	showGameResult:function(result){
+		this.onExit();
+		this.unscheduleAllCallbacks();
+		if(result){
+			cc.log('Game win!!');
+		}
+		else{
+			cc.log('Game fail.');
+		}
+	},
 });
